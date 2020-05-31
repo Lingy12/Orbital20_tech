91f95300f55f8fe5e9f61a7c1d8eaef68d24634c
Lingy12 <russellgy1229@gmail.com>
2020-05-31 19:13:03 +0800

    update

---
+++ ToDoList/ToDoList/View/CheckBoxStyle.swift
@@ -0,0 +1,33 @@
+//
+//  CheckBox.swift
+//  ToDoList
+//
+//  Created by Simon Ng on 24/3/2020.
+//  Copyright © 2020 AppCoda. All rights reserved.
+//
+
+import SwiftUI
+
+struct CheckboxStyle: ToggleStyle {
+    
+    func makeBody(configuration: Self.Configuration) -> some View {
+        
+        return HStack {
+            
+            Image(systemName: configuration.isOn ? "checkmark.circle.fill" : "circle")
+                .resizable()
+                .frame(width: 24, height: 24)
+                .foregroundColor(configuration.isOn ? .purple : .gray)
+                .font(.system(size: 20, weight: .bold, design: .default))
+                .onTapGesture {
+                    configuration.isOn.toggle()
+                }
+            
+            
+            configuration.label
+                
+            
+        }
+        
+    }
+}
